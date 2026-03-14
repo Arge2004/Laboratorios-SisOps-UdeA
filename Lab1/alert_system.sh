2e9b548bd1be44f88ae4b734b00e3f1e75ee894f
Argenis Medina <argemm.dev08@gmail.com>
2026-03-14 18:05:25 -0500

    Refactor alert thresholds to use configurable variables for improved flexibility

--- Lab1/alert_system.sh
+++ Lab1/alert_system.sh
@@ -5,6 +5,18 @@ LOG_DIR="$HOME/system_monitor_logs"
 ALERT_LOG="$LOG_DIR/alerts.log"
 # Archivo temporal para controlar el anti-flooding
 LAST_ALERT_TIME="/tmp/last_alert_check"
+# Cargar configuración (Tarea 2 integración)
+CONFIG_FILE="$HOME/system_monitor_logs/monitor.conf"
+
+# Valores por defecto por si el archivo no existe
+RAM_THRESHOLD=90
+CPU_THRESHOLD=5
+DISK_THRESHOLD=85
+
+# Si el archivo existe, sobrescribe los valores
+if [ -f "$CONFIG_FILE" ]; then
+  source "$CONFIG_FILE"
+fi
 
 # Colores para la consola
 RED='\033[0;31m'
@@ -44,20 +56,20 @@ check_alerts() {
   local ram_usage cpu_load disk_usage interface net_status
   #RAM usage > 90%
   ram_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
-  if (($(echo "$ram_usage > 90" | bc -l))); then
+  if (($(echo "$ram_usage > $RAM_THRESHOLD" | bc -l))); then
     send_alert "WARNING" "RAM usage is high: ${ram_usage}%" "$YELLOW"
   fi
 
   #CPU load average > 5
   cpu_load=$(uptime | awk -F'load average:' '{ print $2 }' | cut -d, -f1 | xargs)
-  if (($(echo "$cpu_load > 5" | bc -l))); then
+  if (($(echo "$cpu_load > $CPU_THRESHOLD" | bc -l))); then
     send_alert "CRITICAL" "CPU Load Average is high: $cpu_load" "$RED"
   fi
 
   #Disk usage > 85% on root partition
   disk_usage=$(df / | grep / | awk '{ print $5 }' | sed 's/%//')
-  if [ "$disk_usage" -gt 85 ]; then
-    send_alert "CRITICAL" "Disk usage on root partition is > 85% ($disk_usage%)" "$RED"
+  if [ "$disk_usage" -gt "$DISK_THRESHOLD" ]; then
+    send_alert "CRITICAL" "Disk usage on root partition is > $DISK_THRESHOLD% ($disk_usage%)" "$RED"
   fi
 
   #Network interface down detection
